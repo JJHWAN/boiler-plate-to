a4051c4200b3e2efda659ce4e6a8d2ead337f7d9
정환주 <pp10202@naver.com>
2022-01-21 00:04:52 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -5,13 +5,42 @@ const app = express()
 const port = 3000
 // back server port 설정
 
+const bodyParser = require('body-parser')
+// User model 가져오기
+const {User} = require("./models/User")
+
+const config = require('./config/key')
+
+// body-Parser가 client에서 오는 정보를 서버에서 분석해서 가져올 수 있도록 함
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extend : true}));
+// application/json
+app.use(bodyParser.json());
+
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://pp10202:ghkswn12@boiler-plate.qta9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(()=>console.log('MongoDB Connected...'))
 .catch(err=> console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) =>{
+  // 회원가입 시 필요한 정보들을 client에서 가져오면
+  // 그 정보들을 데이터 베이스에 저장해준다.
+  
+  // req.body 내부에는 json 형식으로
+  // {id : XXX, password : YYY} 식으로 넘어온다.
+  const user = new User(req.body)
+
+  user.save((err, doc)=>{
+    if(err) return res.json({sucess : false, err})
+    return res.status(200).json({
+      sucess:true
+    })
+  })
 })
 
 app.listen(port, () => {
